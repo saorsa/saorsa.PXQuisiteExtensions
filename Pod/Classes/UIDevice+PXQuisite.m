975e15dec0203a94032d92c8b485cc7967faa8bc
Atanas Dragolov <adragolov@gmail.com>
2015-02-04 13:08:40 +0200

    UIDevice extensions.

--- Pod/Classes/UIDevice+PXQuisite.m
+++ Pod/Classes/UIDevice+PXQuisite.m
@@ -10,4 +10,24 @@
 
 @implementation UIDevice (PXQuisite)
 
+- (NSDictionary *)dictionaryRepresentation {
+
+    return @ {
+      
+        @"name" : self.name ?: [NSNull null],
+        @"model" : self.model ?: [NSNull null],
+        @"localizationModel" : self.localizedModel ?: [NSNull null],
+        @"systemName" : self.systemName ?: [NSNull null],
+        @"systemVersion" : self.systemVersion ?: [NSNull null],
+        @"identifierForVendor" : self.identifierForVendor.UUIDString ?: [NSNull null],
+        @"isMultitaskingSupported": @(self.isMultitaskingSupported),
+        @"isProximityMonitoringEnabled": @(self.isProximityMonitoringEnabled),
+        @"isGeneratingDeviceOrientationNotifications": @(self.isGeneratingDeviceOrientationNotifications),
+        @"isBatteryMonitoringEnabled": @(self.isBatteryMonitoringEnabled),
+        @"userInterfaceIdiom" : @(self.userInterfaceIdiom),
+        @"proximityState" : @(self.proximityState),
+        @"userInterfaceIdiom" : @(self.userInterfaceIdiom),
+    };
+}
+
 @end
